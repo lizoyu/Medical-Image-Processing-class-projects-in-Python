2efd67b13ec07e2663f380febd69fbbc789b415a
Chungyuen Li <lizoyu1@gmail.com>
2016-09-17 21:59:06 -0500

    global threshold

--- Chapter10.py
+++ Chapter10.py
@@ -7,8 +7,28 @@ import numpy as np
 #                                                 #
 ###################################################
 
-def globalThres():
+#Global Threshold
+#NOTE
+# threshold an image according to its global gray scale
+#INPUT
+# img: input image
+#OUTPUT
+# res: threshold image
+def globalThres(img):
+    T = np.mean(img)
+    thres = 0; dT = 1
 
+    while(dT > thres):
+        g1 = img[img > T]
+        g2 = img[img <= T]
+        Tpri = T
+        T = (np.mean(g1) + np.mean(g2)) / 2
+        dT = np.abs(T - Tpri)
+
+    img[img > T] = 255
+    img[img <= T] = 0
+    res = img[:]
+    
     return res
 
 def otsu():
